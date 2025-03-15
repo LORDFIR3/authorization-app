25d1f5ef1f86558db47b753d8e1063a1a91c08bc
LORDFIR3 <kpiataiev@gmail.com>
2025-03-16 00:45:31 +0200

    minor bug fixes

--- app.py
+++ app.py
@@ -78,4 +78,4 @@ def protected():
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()  # Ensures tables are created
-    app.run(debug=True)
+    app.run(host="0.0.0.0",port=5000,debug=True)
